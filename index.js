13635867f39d7507d110618c5152cb933e049b9e
alevscyg <110639375+alevscyg@users.noreply.github.com>
2024-03-29 05:56:11 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require("express")
-const ordersRouts = require('./routes/orders.routes')
-const app = express()
-app.use(express.json())
-app.use('/',ordersRouts)
+const express = require("express");
+const ordersRouts = require('./routes/orders.routes');
+const app = express();
+app.use(express.json());
+app.use('/',ordersRouts);
 
-app.listen(3001,()=> console.log(`server started on port 3001`))
+app.listen(3001,()=> console.log(`server started on port 3001`));
